89c8bb998a2222fdaa4917227134e55fa0ff9a62
benardop <benard.oc.p@gmail.com>
2022-08-20 00:05:06 +0300

    Update webpack-config in the project root folder

--- webpack.config.js
+++ webpack.config.js
@@ -3,22 +3,19 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+
+  },
   devServer: {
     static: './dist',
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-  ],
+
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-  },
-  optimization: {
-    runtimeChunk: 'single',
+
   },
   module: {
     rules: [
@@ -26,7 +23,41 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(gif|png|jpe?g)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/images/',
+            },
+          },
+        ],
+      },
+
+      {
+        test: /\.html$/,
+        use: [
+          'html-loader',
+        ],
+      },
+
     ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+
+      template: './src/index.html',
+    }),
+  ],
+  optimization: {
+    runtimeChunk: 'single',
   },
 
 };
