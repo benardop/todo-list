c96f6cea35fcc609cba47310a293ab198aa3923a
benardop <benard.oc.p@gmail.com>
2022-08-20 00:02:03 +0300

    Add index.js to the src folder

--- src/index.js
+++ src/index.js
@@ -1,13 +1,39 @@
-import _ from 'lodash';
 import './style.css';
+import generateTask from './modules/generateTask.js';
+import checkInteraction from './modules/function-1.js';
+import getTasks from './modules/task.js';
+import isLocalStorageEmpty from './modules/isLocalStorageEmpty.js';
+import addNewTask from './modules/addNewTask.js';
+import removeFuncionality from './modules/removeFunctionality.js';
+import editPreserve from './modules/editPreserve.js';
+import updateCheckBox from './modules/updateCheckBox.js';
+import clearCompleted from './modules/clearCompleted.js';
 
-function component() {
-  const element = document.createElement('div');
+/// ///////////////////////////////////////
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+window.onload = () => {
+  if (isLocalStorageEmpty()) {
+    localStorage.setItem('data', '[]');
+  } else {
+    const tasks = getTasks();
 
-  return element;
-}
+    tasks.sort((b, a) => b.index - a.index);
+    tasks.forEach((el) => {
+      const div = generateTask(el.discription);
+      removeFuncionality(div);
+      editPreserve(div);
+      document.querySelector('.task-list').appendChild(div);
+    });
 
-document.body.appendChild(component());
+    checkInteraction();
+    updateCheckBox();
+  }
+};
+
+document.getElementById('add-new-task').onchange = (e) => {
+  const newTask = e.target.value;
+  addNewTask(newTask);
+  e.target.value = '';
+};
+
+document.querySelector('.end button').onclick = clearCompleted;
